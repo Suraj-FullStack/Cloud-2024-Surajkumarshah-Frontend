7bcfae0a506aacfb4871a95cb591c7c4cef42d43
Suraj Kumar Shah <surajks.shah@gmail.com>
2026-05-05 08:36:07 +0545

    Frontend

---
+++ cloud-24-a-frontend/app/types.ts
@@ -0,0 +1,11 @@
+export type QueryItem = {
+  first_name: string;
+  last_name: string;
+  phone_number: string;
+  query: string;
+};
+
+export const BACKEND_API_URL =
+  "https://cloud2024a-suraj-backend-ergfath0c3hwerg7.centralindia-01.azurewebsites.net/queries";
+ 
+export const APP_API_URL = "/api/queries";
